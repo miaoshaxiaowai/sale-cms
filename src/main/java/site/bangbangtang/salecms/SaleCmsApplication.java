8bfc7601aeeaee5d38a6491eb5f9525bfba9fbb5
Admin <liumingli95@gmail.com>
2020-04-03 23:25:14 +0800

    Initial commit

---
+++ src/main/java/site/bangbangtang/salecms/SaleCmsApplication.java
@@ -0,0 +1,13 @@
+package site.bangbangtang.salecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/bangbangtang/salecms/SaleCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.bangbangtang.salecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
